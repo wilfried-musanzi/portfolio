c3e0068417e60f4665fbb0ca1b87591ccd1055cf
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-14 09:57:45 +0200

    Header: Mobile-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import "../scss/main.scss";
+
+const btnOpen = document.querySelector(".btn-open");
+const btnClose = document.querySelector(".btn-close");
+const header = document.querySelector(".header");
+const sectionHero = document.querySelector(".section--hero");
+
+btnOpen.addEventListener("click", (e) => {
+    e.preventDefault();
+    header.classList.add("nav__open");
+    document.querySelector(".btn-open").classList.add("rotate");
+    sectionHero.classList.add("blur");
+})
+
+btnClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    header.classList.remove("nav__open");
+    sectionHero.classList.remove("blur");
+})
