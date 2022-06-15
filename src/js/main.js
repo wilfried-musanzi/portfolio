df1e2ee0a8aa0143755c83c0e4aa18254ca2f63c
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-15 09:38:16 +0200

    Parcel set up

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,14 @@
-import "../scss/main.scss";
-
 const btnOpen = document.querySelector(".btn-open");
 const btnClose = document.querySelector(".btn-close");
 const header = document.querySelector(".header");
 const sectionHero = document.querySelector(".section--hero");
 
-btnOpen.addEventListener("click", (e) => {
-    e.preventDefault();
+btnOpen.addEventListener("click", () => {
     header.classList.add("nav__open");
     sectionHero.classList.add("blur");
 })
 
-btnClose.addEventListener("click", (e) => {
-    e.preventDefault();
+btnClose.addEventListener("click", () => {
     header.classList.remove("nav__open");
     sectionHero.classList.remove("blur");
 })
