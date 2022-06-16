8a38cb73b317e3b79cec7054cbbffbcb4ebd9dd5
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-16 15:56:05 +0200

    Hide navbar on sectiobHero Click

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,25 @@ const btnOpen = document.querySelector(".btn-open");
 const btnClose = document.querySelector(".btn-close");
 const header = document.querySelector(".header");
 const sectionHero = document.querySelector(".section--hero");
+const body = document.body;
 
-btnOpen.addEventListener("click", () => {
+
+const showMenu = () => {
     header.classList.add("nav__open");
     sectionHero.classList.add("blur");
-})
+    body.classList.add("unscrollable")
+}
 
-btnClose.addEventListener("click", () => {
+const hideMenu = () => {
     header.classList.remove("nav__open");
     sectionHero.classList.remove("blur");
+    body.classList.remove("unscrollable");
+}
+
+btnOpen.addEventListener("click", showMenu);
+btnClose.addEventListener("click", hideMenu);
+
+sectionHero.addEventListener("click", () => {
+    if(header.classList.contains("nav__open"))
+        hideMenu();
 })
