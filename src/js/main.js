4df57b304b6cdb176746a1773a946b9344464188
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-16 16:39:57 +0200

    Smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const btnOpen = document.querySelector(".btn-open");
 const btnClose = document.querySelector(".btn-close");
 const header = document.querySelector(".header");
 const sectionHero = document.querySelector(".section--hero");
+const menu = document.querySelector('.nav__list');
 const body = document.body;
 
 
@@ -21,6 +22,14 @@ btnOpen.addEventListener("click", showMenu);
 btnClose.addEventListener("click", hideMenu);
 
 sectionHero.addEventListener("click", () => {
-    if(header.classList.contains("nav__open"))
+    if (header.classList.contains("nav__open"))
         hideMenu();
 })
+
+menu.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!e.target.classList.contains('nav__link')) return
+    hideMenu()
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+});
