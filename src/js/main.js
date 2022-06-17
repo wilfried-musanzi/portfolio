6cfc698d0b224eac20225785abcdf30c594f29d2
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-17 12:30:09 +0200

    Header animations

--- src/js/main.js
+++ src/js/main.js
@@ -52,6 +52,6 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
     sectionObserver.observe(section);
-    section.classList.add('section--hidden');
 });
+
 
