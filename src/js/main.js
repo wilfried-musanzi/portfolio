e113a7eb0a10abcd1e4e364bd7d97029c23e93ee
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-17 12:19:57 +0200

    Header animations

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const header = document.querySelector(".header");
 const sectionHero = document.querySelector(".section--hero");
 const menu = document.querySelector('.nav__list');
 const body = document.body;
+const nav = document.querySelector(".navbar");
 
 
 const showMenu = () => {
@@ -33,3 +34,24 @@ menu.addEventListener('click', (e) => {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
 });
+
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
