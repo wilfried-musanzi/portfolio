d4df8814e692f1eca0db1260de7b8a6f4150d768
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-06-17 12:39:11 +0200

    Header animations

--- src/js/main.js
+++ src/js/main.js
@@ -36,22 +36,24 @@ menu.addEventListener('click', (e) => {
 });
 
 
-const allSections = document.querySelectorAll('.section');
+const allSections = document.querySelectorAll('.fadeIn');
 
 const revealSection = function (entries, observer) {
     const [entry] = entries;
     if (!entry.isIntersecting) return;
-    entry.target.classList.remove('section--hidden');
+    entry.target.classList.remove('hidden');
     observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
     root: null,
-    threshold: 0.15,
+    threshold: 0.1,
 });
 
 allSections.forEach(function (section) {
     sectionObserver.observe(section);
+   section.classList.add('hidden');
+
 });
 
 
